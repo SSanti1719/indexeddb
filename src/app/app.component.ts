7a8e9fe9f95111a6bb62dd92493ab02ed06d6258
Santiago Salazar Parra <santiago.1701913303@ucaldas.edu.co>
2021-12-09 10:07:59 -0500

    ejemplo indexed db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { liveQuery } from 'dexie';
+import { db, TodoList } from '../db/db';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pruebaIndexed';
+  todoLists$ = liveQuery(() => db.todoLists.toArray());
+  listName = 'My new list';
+
+  async addNewList() {
+    await db.todoLists.add({
+      title: this.listName,
+    });
+  }
+
+  async resetDatabase() {
+    await db.resetDatabase();
+  }
+
+  identifyList(index: number, list: TodoList) {
+    return `${list.id}${list.title}`;
+  }
 }
